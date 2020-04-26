0da03a564bfaa0d2d48619e116b178e493f4e9e9
zpyang2920 <zpyang2920@gmail.com>
2020-04-26 13:04:36 -0500

    assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -41,6 +42,7 @@ class Main extends Component {
             );
           };
 
+
         return (
             <div>
                 <Header />
@@ -48,7 +50,8 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={Contact} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path="/aboutus" component={ () =>  <About leaders={this.state.leaders} />  } />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
